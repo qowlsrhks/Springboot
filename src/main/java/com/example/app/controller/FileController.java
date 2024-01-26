0f92299428cddccdc51b57b14abaff933d4c079b
BAE JINGWAN <skswlsrhks@naver.com>
2024-01-26 17:17:21 +0900

    day3

--- src/main/java/com/example/app/controller/FileController.java
+++ src/main/java/com/example/app/controller/FileController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -13,7 +17,18 @@ import java.util.List;
 @Slf4j
 public class FileController {
     @PostMapping("upload")
-    public void upload(List<MultipartFile> upload) {
+    public void upload(List<MultipartFile> upload) throws IOException {
         String rootPath = "C:/upload";
+        File uploadPath = new File(rootPath, creatDirectoryToday());
+        if(!uploadPath.exists()){uploadPath.mkdirs();}
+
+        for (MultipartFile multipartFile : upload) {
+            File save = new File(uploadPath, multipartFile.getOriginalFilename());
+            multipartFile.transferTo(save);
+        }
+    }
+
+    private String creatDirectoryToday(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
     }
 }
